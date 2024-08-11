d0c17f9468abf4f0bd16d76883196b6fb55774b3
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-11 15:16:53 +0300

    #first

---
+++ checkout-UI/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { BodyComponent } from './Components/body/body.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    BodyComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
