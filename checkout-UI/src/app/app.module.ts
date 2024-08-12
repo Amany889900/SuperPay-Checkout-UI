6b66fa71380a45697d6c76a210d19ddbd18aa4d9
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-13 02:07:50 +0300

    #second

--- checkout-UI/src/app/app.module.ts
+++ checkout-UI/src/app/app.module.ts
@@ -5,21 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BodyComponent } from './Components/body/body.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
+
 import { FrameComponent } from './Components/frame/frame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
-    HeaderComponent,
-    FooterComponent,
     FrameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration()
