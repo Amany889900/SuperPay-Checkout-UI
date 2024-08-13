b24da78b1c62b74aae4103a1a70d1ff5a559f2bd
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-13 14:49:10 +0300

    #mat-expansion

--- checkout-UI/src/app/app.module.ts
+++ checkout-UI/src/app/app.module.ts
@@ -7,22 +7,26 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './Components/body/body.component';
 
 import { FrameComponent } from './Components/frame/frame.component';
-
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
     FrameComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
