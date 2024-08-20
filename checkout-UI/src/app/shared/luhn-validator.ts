97c5cdea6ac08dfe6d616446fa291eadf4493e18
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-21 00:27:27 +0300

    Done

--- checkout-UI/src/app/shared/luhn-validator.ts
+++ checkout-UI/src/app/shared/luhn-validator.ts
@@ -1,16 +1,13 @@
-import { AbstractControl} from '@angular/forms';
+import { AbstractControl } from "@angular/forms";
 
-export function luhnValidator(control: AbstractControl):{[key:string]:boolean} | null {
- 
+export function luhnValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  
   let sum = 0;
   let shouldDouble = false;
+  const value = control.value.replace(/\s+/g, ''); // Remove spaces
 
-  for (let i = control.value.length - 1; i >= 0; i--) {
-    let digit = parseInt(control.value.charAt(i), 10);
-
-    if (isNaN(digit)) {
-        continue;
-      }
+  for (let i = value.length - 1; i >= 0; i--) {
+    let digit = parseInt(value.charAt(i), 10);
 
     if (shouldDouble) {
       digit *= 2;
