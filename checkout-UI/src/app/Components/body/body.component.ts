148dbd0218e0b18bf44a808c9d7303948ff2f0dc
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-20 16:37:33 +0300

    third

--- checkout-UI/src/app/Components/body/body.component.ts
+++ checkout-UI/src/app/Components/body/body.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from '../../shared/user-name.validator';
+import { PasswordValidator } from '../../shared/password.validator';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
+  registrationForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    },{validator: PasswordValidator});
+  }
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: 'Amany',
+      password: 'test',
+      confirmPassword: 'test',
+    });
+  }
 }
