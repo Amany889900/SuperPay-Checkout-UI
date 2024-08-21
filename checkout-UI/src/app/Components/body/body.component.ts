860ae9371240c7b506bf30e9a6a621b8972f5d94
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-08-21 16:45:04 +0300

    initial

--- checkout-UI/src/app/Components/body/body.component.ts
+++ checkout-UI/src/app/Components/body/body.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CustomComponent } from '../../customized/custom.component';
+import { item } from '../../Models/item';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-  
+  items:item[];
 
-
-
-constructor(){
-  
+  constructor(){
+    this.items=[{title:'header1',content:'content1'},
+      {title:'header2',content:'content2'},
+      {title:'header3',content:'content3'}
+    ]
 }
 
 }
---
+++ checkout-UI/src/app/Models/item.ts
@@ -0,0 +1,6 @@
+export interface item {
+   
+    title:string;
+    content:string;
+  
+}
